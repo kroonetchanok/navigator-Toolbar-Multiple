5800e9c21e5bcd8d91d1d147566584157bcc4038
Student12 <Student12@Student12s-iMac.local>
2017-06-14 16:44:34 +0700

    create secondviewcontroll

--- Navigator Toolbar Multiple Screen/ViewController.swift
+++ Navigator Toolbar Multiple Screen/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+  //Implicit
+    var intNumber = 1
+    
+    
+    
+    
+    @IBAction func increaseNumber(_ sender: Any) {
+     showMessage(strMessange: "Click increaseNumber")
+     intNumber += 1
+        intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+    }
+    
+    
+    @IBAction func decreseNumber(_ sender: Any) {
+    
+      showMessage(strMessange: "Click decreseNumber")
+      intNumber -= 1
+        intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+    }
+    
+    
+    @IBAction func home(_ sender: Any) {
+      showMessage(strMessange: "Click home")
+      intNumber = 1
+        showNumber(intNumber: intNumber)
+    }
+    
+    
+    @IBOutlet weak var NumberLabel: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }  // Main Method
+    
+    func checkNumber(intNumber: Int) -> Int {
+     // var intResult = intNumber
+        
+        // At 11
+        if intNumber == 11
+        {
+           // intResult == -10
+        }
+        // at -11
+        if intNumber == -11  {
+          //  intResult == 10
+        }
+        
+        return 123
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+func showNumber(intNumber: Int) -> Void {
+    let strNumber = String(intNumber)
+    NumberLabel.text = strNumber
+    
 }
+
+    func showMessage(strMessange: String) -> Void{
+            print("Message ==> \(strMessange)")
+    }
+}
+
+    //override func didReceiveMemoryWarning() {
+      //  super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+ //   }
+
+
+//}
 
